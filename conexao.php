90da1ab04fd37007f66771f8644386ec7f325569
Cjota23 <ca079133@gmail.com>
2024-11-11 08:13:44 -0300

    vai pelo amor de Deus

--- conexao.php
+++ conexao.php
@@ -1,12 +1,18 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Seu usuário do MySQL
-$password = ""; // Sua senha do MySQL
-$dbname = "crud_login"; // Nome do banco de dados
+$username = "root"; 
+$password = ""; 
+$dbname = "confeitaria"; 
 
+// Criação da conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verificação de conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
+} else {
+    echo "Conexão bem-sucedida!";
 }
+
+// Utilize essa conexão ($conn) para realizar consultas ao banco de dados
 ?>
